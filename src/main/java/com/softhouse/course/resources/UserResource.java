ff143d9f68641cd4d4c74fa9bd328552e86aa941
jaquesonw <jaquesonw@gmail.com>
2022-12-01 10:55:28 -0300

    User entity and resource

---
+++ src/main/java/com/softhouse/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.softhouse.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softhouse.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "8491922856", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
