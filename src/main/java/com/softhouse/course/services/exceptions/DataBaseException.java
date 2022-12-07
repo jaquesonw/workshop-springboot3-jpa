71abd1478f3aedf2428d9b33cfebea8d56987c68
jaquesonw <jaquesonw@gmail.com>
2022-12-07 10:38:00 -0300

    Exception handling - update

--- src/main/java/com/softhouse/course/services/exceptions/DataBaseException.java
+++ src/main/java/com/softhouse/course/services/exceptions/DataBaseException.java
@@ -3,7 +3,7 @@ package com.softhouse.course.services.exceptions;
 public class DataBaseException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public DataBaseException(String msg) {
 		super(msg);
 	}
