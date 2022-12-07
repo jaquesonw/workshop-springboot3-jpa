88586109741ccc78648c2fc9b864469eef903ca4
jaquesonw <jaquesonw@gmail.com>
2022-12-07 10:21:04 -0300

    Exception handling - findById

--- src/main/java/com/softhouse/course/resources/UserResource.java
+++ src/main/java/com/softhouse/course/resources/UserResource.java
@@ -4,7 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;import org.springframework.http.StreamingHttpOutputMessage.Body;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
